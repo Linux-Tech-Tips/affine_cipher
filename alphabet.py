9e7b0377fafba1e2512b197430474ed6563c29ea
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2024-09-30 13:45:22 +0100

    Added new alphabets

--- alphabet.py
+++ alphabet.py
@@ -29,6 +29,13 @@ _std_list = [
 def standard(arg):
 	return _alphabetList(arg, _std_list)
 
+_std_list_2 = [
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
+
+def standard2(arg):
+	return _alphabetList(arg, _std_list_2)
+
 # Extended alphabet list
 _ext_list = _std_list + [
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -40,3 +47,13 @@ _ext_list = _std_list + [
 def extended(arg):
 	return _alphabetList(arg, _ext_list)
 
+_27_list = ["!", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+def ext27(arg):
+	return _alphabetList(arg, _27_list)
+
+
+_37_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"]
+
+def ext37(arg):
+	return _alphabetList(arg, _37_list)
